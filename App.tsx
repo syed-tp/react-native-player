d0e8344d326a99a0fd2c2aef9e3c82c160165d90
syed-tp <syed@testpress.in>
2025-04-19 13:45:35 +0530

    add text

--- App.tsx
+++ App.tsx
@@ -43,6 +43,7 @@ function App(): React.JSX.Element {
 
   return (
       <View style={styles.container}>
+        <Text> Video player</Text>
         <VideoPlayer
           ref={playerRef}
           endWithThumbnail
