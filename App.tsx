50b1012a4b8e768c95246f4af183b9b2bbfb56ec
syed-tp <syed@testpress.in>
2025-04-19 11:14:41 +0530

    chore: remove player ovrelay text

--- App.tsx
+++ App.tsx
@@ -55,27 +55,6 @@ function App(): React.JSX.Element {
           onError={(e) => console.log(e)}
           showDuration={true}
         />
-        <Text style={[styles.watermarkText, position]}>Watermark Text</Text>
-
-        <View style={styles.buttonContainer}>
-        <Text style={styles.text}>Apply watermark on</Text>
-          <View style={styles.buttonRow}>
-            <View style={styles.buttonWrapper}>
-              <Button title="Top Left" onPress={() => moveWatermark('top-left')} />
-            </View>
-            <View style={styles.buttonWrapper}>
-              <Button title="Top Right" onPress={() => moveWatermark('top-right')} />
-            </View>
-          </View>
-          <View style={styles.buttonRow}>
-            <View style={styles.buttonWrapper}>
-              <Button title="Bottom Left" onPress={() => moveWatermark('bottom-left')} />
-            </View>
-            <View style={styles.buttonWrapper}>
-              <Button title="Bottom Right" onPress={() => moveWatermark('bottom-right')} />
-            </View>
-          </View>
-        </View>
       </View>
   );
 }
@@ -90,30 +69,6 @@ const styles = StyleSheet.create({
     fontSize:30,
     color: 'white',
     fontWeight: 'bold',
-  },
-  watermarkText: {
-    position: 'absolute',
-    right: 10,
-    top: 10, 
-    fontSize: 25,
-    color: 'white',
-    fontWeight: 'bold',
-    opacity:0.7,
-  },
-  buttonContainer: {
-    position: 'absolute',
-    bottom: 400,
-    left: 0,
-    right: 0,
-    alignItems: 'center',
-  },
-  buttonRow: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  buttonWrapper: {
-    flex: 1,
-    margin: 5,
   },
 });
 
